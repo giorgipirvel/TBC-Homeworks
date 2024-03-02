1702049b57fabce6a2c95e4730423e54ae1f21ff
giorgipirvel <gio.pirveli001@gmail.com>
2024-03-02 14:31:59 +0400

    HWs

--- Homewrok_1/task_3.py
+++ Homewrok_1/task_3.py
@@ -1,10 +1,11 @@
-a=float(input("Enter the length of side a: "))
-b=float(input("Enter the length of side b: "))
-c=float(input("Enter the length of side c: "))
+a=int(input("Enter the length of side a: "))
+b=int(input("Enter the length of side b: "))
+c=int(input("Enter the length of side c: "))
+s=(a+b+c)/2
 
 if a+b>c and b+c>a and a+c>b:
     print(a+b+c)
-    print((a+b+c)/2)
+    print((s*(s-a)*(s-b)*(s-c))**0.5)
 
 else:
     print("violation")
