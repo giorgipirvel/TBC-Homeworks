a5444c2bb91e0d576598ba0b48fc363d1dc099b9
giorgipirvel <gio.pirveli001@gmail.com>
2024-03-06 17:45:34 +0400

    HWs

--- Homewrok_1/task_1.py
+++ Homewrok_1/task_1.py
@@ -1,5 +1,8 @@
 name=input("What's your name? ")
 surname=input("What's your surname? ")
-age=input("How old are you? ")
+age=int(input("How old are you? "))
 
-print("Hello,",name, surname, "your age is",age)
+if age>0:
+    print("Hello,",name, surname, "your age is",age)
+else:
+    print("It's impossible")
--- Homewrok_1/task_3.py
+++ Homewrok_1/task_3.py
@@ -4,8 +4,8 @@ c=int(input("Enter the length of side c: "))
 s=(a+b+c)/2
 
 if a+b>c and b+c>a and a+c>b:
-    print(a+b+c)
-    print((s*(s-a)*(s-b)*(s-c))**0.5)
+    print(a+b+c)  #Perimeter
+    print((s*(s-a)*(s-b)*(s-c))**0.5)   #Area
 
 else:
     print("violation")
