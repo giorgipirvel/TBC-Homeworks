a6d97998549f0f6205574fbae1080f9332467c5e
giorgipirvel <161208294+giorgipirvel@users.noreply.github.com>
2024-03-06 18:18:36 +0400

    Revert "Finished first homework"

--- Homework_3/task_1.py
+++
@@ -1,6 +0,0 @@
-import math
-
-x=int(input("Enter the number: "))
-y=int(input("Enter the number: "))
-
-print(math.pow(x, y))
--- Homewrok_1/task_1.py
+++
@@ -1,8 +0,0 @@
-name=input("What's your name? ")
-surname=input("What's your surname? ")
-age=int(input("How old are you? "))
-
-if age>0:
-    print("Hello,",name, surname, "your age is",age)
-else:
-    print("It's impossible")
