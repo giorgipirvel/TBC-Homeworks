39561d2b8bde50165f7d250b5934db2b87f918b9
giorgipirvel <161208294+giorgipirvel@users.noreply.github.com>
2024-03-07 18:27:18 +0400

    Delete Homework_2 directory

--- Homework_2/task_1.py
+++
@@ -1,6 +0,0 @@
-user_input=input("choose true or false: ")
-
-if user_input=="true":
-    print("whoala")
-else:
-    print("not yet")
--- Homework_2/task_2.py
+++
@@ -1,7 +0,0 @@
-A=int(input("Enter the number: "))
-B=int(input("Enter the number: "))
-
-if A%B==0: #თუ უნაშთოდ გაიყო ესეიგი ჯერადია
-    print("A არის B რიცხვის ჯერადი")
-else:
-    print("A არ არის B რიცხვის ჯერადი")
