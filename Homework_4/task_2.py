8c871306d533b00f3c220272f86a9c00a7b25b2e
giorgipirvel <gio.pirveli001@gmail.com>
2024-03-15 12:44:29 +0400

    changes

--- Homework_4/task_2.py
+++ Homework_4/task_2.py
@@ -6,7 +6,14 @@ if 0 < n < 30:
     random_numbers = [random.randrange(0, 1000) for int in range(n)]
     
     print("Generated numbers:", random_numbers)
-    print("Maximum number:", max(random_numbers))
+   
+    max_number = random_numbers[0]
+    
+    for number in random_numbers:
+        if number > max_number:
+            max_number = number
+
+    print("Maximum number:", max_number)
 
 else: #out of range
     print("N/A")
--- Homework_4/task_3.py
+++ Homework_4/task_3.py
@@ -1,7 +1,7 @@
-n=int(input("Enter the number: "))
+n = int(input("Enter the number: "))
 
-if 0<n<1000:
-    divisor=[i for i in range(1,n+1) if n % i==0]
+if 0 < n < 1000:
+    divisor = [i for i in range(1,n+1) if n % i == 0]
     print(divisor)
 
 else:
