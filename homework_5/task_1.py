54bc21f115d2a2fe0803607bf9adbf43663fb50e
giorgipirvel <gio.pirveli001@gmail.com>
2024-04-14 21:31:16 +0400

    modified

--- homework_5/task_1.py
+++ homework_5/task_1.py
@@ -3,12 +3,12 @@ n=int(input("Enter the number: "))
 if 0 < n < 50:
     for j in range (1,n+1):
         divisors=[1]
-        for i in range (2,(j//2)+1):
+        for i in range (2, j + 1):
              if j % i ==0:
                   divisors.append(i)
                   if len(divisors)==3:
                        break
-        print(f"{j} - {' '.join(map(str, divisors))}")
+        print(f"{j} - {divisors}")
 
 else: 
      print("Out of range")
--- homework_5/task_3.py
+++ homework_5/task_3.py
@@ -1,16 +1,15 @@
 n = int(input("Enter the height of the Christmas tree: "))
 
 if 0 < n < 50:
-    for i in range(1, n + 1):
+    star = " " * n
+    print(f"{star}*")
+    for i in range(1, n):
         spaces = " " * (n - i)
-        stars = "*" * (2 * i - 1)
-        print(f"{spaces}{stars}")
+        branches = ("/" * i) + "|" + ("\\" * i)
+        print(f"{spaces}{branches}")
 
-    bottom = " " * (n - 1)
+    bottom = " " * n 
     print(f"{bottom}|")
 
 else:
     print("Enter number between 0 and 50")
-
-# ვიცი ისეთი არაა როგორიც უნდა იყოს, ვცდი ისეთის შექმნასაც
-        
